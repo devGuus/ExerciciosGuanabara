4594a0483373eeb5cb8c6beb9e90354abbf5f463
GuusMotoBoy <gustavoxavi22@gmail.com>
2023-04-16 00:47:03 -0300

    first commit

---
+++ Exercicio1.py
@@ -0,0 +1,2 @@
+nome = input("Digite o seu nome: ")
+print("Obrigado {}! por estar visitando minha pagina no Github!" .format(nome))
---
+++ exercicio4.py
@@ -0,0 +1,2 @@
+x = int(input("Digite um número: "))
+print(f'Analisando o valor {x}, o seu antecessor é {x-1} e o sucessor é {x+1}')
