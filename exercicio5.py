7964d970b11af02fb4a767d0f4552cad30e981be
GuusMotoBoy <gustavoxavi22@gmail.com>
2023-04-16 01:07:12 -0300

    New Files

---
+++ exercicio5.py
@@ -0,0 +1,3 @@
+valor = int(input('Digite um valor: '))
+print(f'O dobro de {valor} é {valor*2}\nO triplo de {valor} é {valor*3}' )
+print('A raiz quadrada de {} é {:.2f}' .format(valor, valor ** (1/2)))
